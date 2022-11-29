55a95384dedaeed0c9a4e9847958e08f4854f121
Amber <amberluckyjob@protonmail.com>
2022-11-29 17:48:08 +0800

    左边样式
    左边样式

--- components/PageLeft.vue
+++ components/PageLeft.vue
@@ -1,28 +1,17 @@
 <template>
-  <div class="left"></div>
+  <div class="nav"></div>
 </template>
 
 <script>
 export default {
   name: 'PageLeft',
   data() {
-    return {
-      menu: [],
-    }
+    return {}
   },
 }
 </script>
 <style lang="scss" scoped>
 @use 'sass:math';
-$block: 140px;
-$spacing: 16px;
-.left {
-  position: fixed;
-  left: 0;
-  top: 0;
-  width: 255px;
-  height: 100%;
-  background-color: #1f2128;
-  box-shadow: 1px 0px 0px 0px #2e313d;
+.nav {
 }
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="main">
-    <page-header></page-header>
-    <Nuxt />
+    <page-left class="main__left"></page-left>
+    <Nuxt class="main__page" />
   </main>
 </template>
 
@@ -14,7 +14,18 @@ export default {
 </script>
 <style lang="scss" scoped>
 .main {
-  display: flex;
   min-height: 100vh;
+  &__left {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 255px;
+    height: 100%;
+    background-color: #1f2128;
+    box-shadow: 1px 0px 0px 0px #2e313d;
+  }
+  &__page {
+    padding-left: 255px;
+  }
 }
 </style>
