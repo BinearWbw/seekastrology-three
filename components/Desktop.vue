69f4a88cb13086bd7f2d2d22ed53612afa3ede02
Amber <amberluckyjob@protonmail.com>
2023-02-17 11:10:09 +0800

    修改install
    修改install

--- components/Desktop.vue
+++ components/Desktop.vue
@@ -1,8 +1,10 @@
 <template>
-  <button class="desktop common__btn" v-if="prompt" @click="download">
-    <span class="pc">TO DESKTOP</span>
-    <span class="h5">INSTALL</span>
-  </button>
+  <div class="install">
+    <button class="desktop common__btn" v-if="prompt" @click="download">
+      <span class="pc">TO DESKTOP</span>
+      <span class="h5">INSTALL</span>
+    </button>
+  </div>
 </template>
 <script>
 export default {
@@ -40,42 +42,47 @@ export default {
 </script>
 <style lang="scss" scoped>
 @use 'sass:math';
-.desktop {
+.install {
   margin-left: 20px;
   -webkit-flex-shrink: 0;
   flex-shrink: 0;
   width: 110px;
-  height: 34px;
-  background: linear-gradient(90deg, #c14afe 0%, #7a79ff 100%), #6c5dd3;
-  border-radius: 6px;
-  text-align: center;
-  line-height: 34px;
-  font-size: 12px;
-  &:hover {
-    background: linear-gradient(90deg, #cb68fc 0%, #8484fd 100%), #6c5dd3;
-  }
-  .h5 {
-    display: none;
+  .desktop {
+    width: 100%;
+    height: 34px;
+    background: linear-gradient(90deg, #c14afe 0%, #7a79ff 100%), #6c5dd3;
+    border-radius: 6px;
+    text-align: center;
+    line-height: 34px;
+    font-size: 12px;
+    &:hover {
+      background: linear-gradient(90deg, #cb68fc 0%, #8484fd 100%), #6c5dd3;
+    }
+    .h5 {
+      display: none;
+    }
   }
 }
 @media (max-width: (1600px)) {
-  .desktop {
+  .install {
     width: 96px;
   }
 }
 @media (max-width: 750px) {
   $pr: math.div(1vw, 3.75);
-  .desktop {
+  .install {
     width: 96 * $pr;
-    height: 34 * $pr;
-    border-radius: 6 * $pr;
-    line-height: 36 * $pr;
-    font-size: 10 * $pr;
-    .pc {
-      display: none;
-    }
-    .h5 {
-      display: block;
+    .desktop {
+      height: 34 * $pr;
+      border-radius: 6 * $pr;
+      line-height: 36 * $pr;
+      font-size: 10 * $pr;
+      .pc {
+        display: none;
+      }
+      .h5 {
+        display: block;
+      }
     }
   }
 }
