d88aa7c8cd045dbe3d31f00a858080a839834d0f
BinearWbw <binear7@gmail.com>
2023-06-05 18:33:12 +0800

    首页接口对接

--- components/home/Various.vue
+++ components/home/Various.vue
@@ -6,8 +6,8 @@
     </div>
     <ul class="various__main">
       <li v-for="(item, index) in divination" :key="index">
-        <a class="various__main__li" href="#">
-          <img src="~/assets/img/home/stars.png" alt="stars" />
+        <a class="various__main__li" :href="`${getIntersperseUrl + item.path}`">
+          <img :src="item.imgUrl" alt="stars" />
           <p class="title">{{ item.title }}</p>
           <p class="text">{{ item.textCont }}</p>
           <button class="button">Read More</button>
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'Various',
   data() {
@@ -27,29 +28,44 @@ export default {
         {
           title: 'Horoscope',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/horoscope.png'),
+          path: '/horroscope',
         },
         {
           title: 'Zodiac Signs',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/zodiac.png'),
+          path: '/zodiac',
         },
         {
           title: 'Tarot',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/tarot.png'),
+          path: '#',
         },
         {
           title: 'Compatibility',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/compatibility.png'),
+          path: '/astrology',
         },
         {
           title: 'Quizzes',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/quizzes.png'),
+          path: '#',
         },
         {
           title: 'Articles',
           textCont: 'Read your freedaily horoscopes and learn more about your',
+          imgUrl: require('~/assets/img/home/articles.png'),
+          path: '/resources',
         },
       ],
     }
+  },
+  computed: {
+    ...mapGetters(['getIntersperseUrl']),
   },
 }
 </script>
@@ -89,7 +105,7 @@ export default {
     &__li {
       height: 100%;
       display: flex;
-      padding-top: 44px;
+      padding-top: 34px;
       display: -webkit-box;
       display: -webkit-flex;
       display: -ms-flexbox;
@@ -110,10 +126,10 @@ export default {
       -webkit-transition: transform 0.3s ease-in-out;
       transition: transform 0.3s ease-in-out;
       img {
-        width: 132px;
-        height: 132px;
+        width: 152px;
+        height: 152px;
         object-fit: cover;
-        margin-bottom: 24px;
+        margin-bottom: 14px;
       }
       .title {
         font-size: 22px;
@@ -176,17 +192,16 @@ export default {
         height: 100%;
       }
       &__li {
-        padding: 24 * $pr 0;
+        padding: 13 * $pr 0 7 * $pr;
         img {
-          width: 118 * $pr;
-          height: 118 * $pr;
+          width: 93 * $pr;
+          height: 93 * $pr;
           object-fit: cover;
-          margin-bottom: 11 * $pr;
+          margin-bottom: 9 * $pr;
         }
         .title {
           font-size: 16 * $pr;
           line-height: 22 * $pr;
-          margin-bottom: 8 * $pr;
           padding: 0 10 * $pr;
         }
         .text {
--- components/home/YourChoice.vue
+++ components/home/YourChoice.vue
@@ -5,11 +5,19 @@
       <p>Read Your Daily Horoscope Right Now</p>
     </div>
     <ul class="choice__main">
-      <li v-for="item in constellation" :key="item.title">
+      <li v-for="item_i in item" :key="item_i.id">
         <a class="choice__main__a" href="#">
-          <img class="img__list" :src="item.imgList" :alt="item.title" />
-          <p class="title">{{ item.title }}</p>
-          <p class="time">{{ item.time }}</p>
+          <div class="img__list">
+            <nuxt-img
+              :src="item_i.icon || '/'"
+              fit="cover"
+              width="218"
+              height="154"
+              :alt="item_i.name"
+            ></nuxt-img>
+          </div>
+          <p class="title">{{ item_i.name }}</p>
+          <p class="time">{{ item_i.dates }}</p>
         </a>
       </li>
     </ul>
@@ -19,71 +27,9 @@
 <script>
 export default {
   name: 'YourChoice',
+  props: ['item'],
   data() {
-    return {
-      constellation: [
-        {
-          imgList: require('~/assets/img/home/choice/Aries.png'),
-          title: 'Aries',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Taurus.png'),
-          title: 'Taurus',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Gemini.png'),
-          title: 'Gemini',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Cancer.png'),
-          title: 'Cancer',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Leo.png'),
-          title: 'Leo',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Virgo.png'),
-          title: 'Virgo',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Libra.png'),
-          title: 'Libra',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Scorpio.png'),
-          title: 'Scorpic',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Sagittarius.png'),
-          title: 'Sagittarius',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Capricorn.png'),
-          title: 'Capricorn',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Aquarius.png'),
-          title: 'Aquarius',
-          time: 'Mar 21 - Apr 20',
-        },
-        {
-          imgList: require('~/assets/img/home/choice/Pisces.png'),
-          title: 'Pisces',
-          time: 'Mar 21 - Apr 20',
-        },
-      ],
-    }
+    return {}
   },
 }
 </script>
@@ -128,8 +74,14 @@ export default {
         .img__list {
           width: 218px;
           height: 154px;
-          object-fit: cover;
-          transition: transform 0.3s;
+          border-radius: 6px;
+          overflow: hidden;
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            transition: transform 0.3s;
+          }
         }
         .title {
           font-family: 'Rubik';
@@ -149,7 +101,9 @@ export default {
       &:hover {
         border-color: rgba(255, 255, 255, 0.6);
         .img__list {
-          transform: scale(1.2);
+          img {
+            transform: scale(1.2);
+          }
         }
       }
     }
@@ -187,7 +141,12 @@ export default {
           .img__list {
             width: 100%;
             height: 100 * $pr;
-            object-fit: contain cover;
+            margin-bottom: auto;
+            img {
+              width: 100%;
+              height: 100%;
+              object-fit: contain cover;
+            }
           }
           .title {
             font-size: 16 * $pr;
