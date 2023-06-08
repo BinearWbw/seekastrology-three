2dc920f8916b5419d9f6d5c73199e4513b092dc4
jintao <542345709@qq.com>
2023-06-08 10:37:42 +0800

    update

--- config/routes.js
+++ config/routes.js
@@ -7,14 +7,12 @@ const addRoutes = async () => {
   res2.data.data.list.map((item) => {
     routes.push(`/resources/details/${item.name.replace(/[^a-zA-Z0-9\\s]/g, '-').toLowerCase()}-${item.id}/`)
   })
-  // let res1 = await axios.get(
-  //   'https://api.taptogame.com/api/sitemap/games?origin=taptogame'
-  // )
-  // res1.data.data.map((item) => {
-  //   const encodedName = item.name.replace(/[^a-zA-Z0-9\\s]/g, '-').toLowerCase()
-  //   routes.push(`/game/${encodedName}-${item.id}`)
-  //   routes.push(`/download/${encodedName}-${item.id}`)
-  // })
+  let res1 = await axios.get(
+    'http://47.108.105.189:3000/api/quiz/all?origin=seekastrology&page=1&size=10000'
+  )
+  res1.data.data.list.map((item) => {
+    routes.push(`/test/details/${item.name.replace(/[^a-zA-Z0-9\\s]/g, '-').toLowerCase()}-${item.id}/`)
+  })
   return routes
 }
 let routes = addRoutes()
