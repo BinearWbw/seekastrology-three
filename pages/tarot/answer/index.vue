8c38bc95c7ad50c58ff9330f6b90d3cfd4f5acdb
BinearWbw <binear7@gmail.com>
2023-06-19 17:37:13 +0800

    塔罗answer页样式整理

--- pages/tarot/answer/index.vue
+++ pages/tarot/answer/index.vue
@@ -1,14 +1,7 @@
-<!--
- * @Date: 2023-06-06 16:51:37
- * @LastEditors: tian 249682049@qq.com
- * @LastEditTime: 2023-06-15 17:19:28
- * @FilePath: /seekastrology/pages/tarot/answer/index.vue
- * @Description: 
--->
 <template>
   <div class="tarot-container">
     <div class="title">What the Tarot Cards Mean in This Reading</div>
-    <div class="ad-box_row mt-48"></div>
+    <google-ad class="ad-box_row mt-48 title_ad"></google-ad>
     <div class="tarot-section">
       <!-- <div class="ad-box"></div> -->
       <div class="main-content">
@@ -38,18 +31,20 @@
       </div>
       <!-- <div class="ad-box"></div> -->
     </div>
-    <div class="ad-box_row mt-48"></div>
+    <google-ad class="google_ad"></google-ad>
     <more-tarot class="mt-48"></more-tarot>
-    <div class="ad-box_row mt-32 mb-32"></div>
+    <google-ad class="google_ad"></google-ad>
   </div>
 </template>
 
 <script>
+import GoogleAd from '../../../components/GoogleAd.vue'
 import MoreTarot from '../../../components/tarot/MoreTarot.vue'
 export default {
   name: 'answer',
   components: {
     MoreTarot,
+    GoogleAd,
   },
   data() {
     return {
@@ -81,7 +76,7 @@ export default {
     }
   },
   mounted() {
-    this.type = this.$route.query.type || '4';
+    this.type = this.$route.query.type || '4'
     const cardsInfo = sessionStorage.getItem('cardsInfo')
     if (cardsInfo) {
       this.cardsInfo = JSON.parse(cardsInfo)
@@ -97,147 +92,183 @@ export default {
 @use 'sass:math';
 .tarot-container {
   color: #fff;
-}
-.title {
-  font-family: 'Cinzel Decorative';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 46px;
-  line-height: 64px;
-  text-align: center;
-}
-
-.tarot-section {
-  display: flex;
-  justify-content: center;
-  .ad-box {
-    width: 160px;
-    height: 600px;
-    background-color: #555761;
-  }
-}
-.ad-box_row {
-  width: 924px;
-  height: 114px;
-  margin: 0 auto;
-  background-color: #555761;
-}
-.main-content {
-  margin: 124px 32px 0;
-  flex: 1;
-  max-width: 1400px;
-  .content-list-item {
-    display: flex;
-    &:not(:first-child) { 
-      margin-top: 97px;
-    }
-    .card-wrapper {
-      margin-right: 137px;
-    }
-    .card-img {
-      height: 390px;
-      width: 220px;
-    }
-    .card-img-rotate {
-      transform: rotate(180deg);
-    }
-    .card-text {
-      text-align: center;
-      font-family: 'Rubik';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 22px;
-      line-height: 30px;
-      color: rgba(255, 255, 255, 0.7);
-      margin-top: 12px;
-    }
-    .desc-title {
-      font-family: 'Cinzel Decorative';
-      font-style: normal;
-      font-weight: 700;
-      font-size: 36px;
-      line-height: 48px;
-    }
-    .desc-text {
-      font-family: 'Rubik';
-      font-style: normal;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 28px;
-      margin-top: 40px;
-    }
-  }
-}
-.mt-48 {
-  margin-top: 48px;
-}
-.mt-32 {
-  margin-top: 32px;
-}
-.mb-32 {
-  margin-bottom: 32px;
-}
-.mt-200 {
-  margin-top: 200px;
-}
-@media (max-width: 750px) {
-  $pr: math.div(1vw, 3.75);
-  .tarot-container {
-    padding: 16 * $pr;
-  }
-  .ad-box_row {
-    width: 100%;
-    height: 300 * $pr;
-  }
+  padding: 40px 0 0;
   .title {
     font-family: 'Cinzel Decorative';
     font-style: normal;
     font-weight: 700;
-    font-size: 36 * $pr;
-    line-height: 48 * $pr;
+    font-size: 46px;
+    line-height: 64px;
+    text-align: center;
+    padding: 0 30px;
   }
+
   .tarot-section {
+    display: flex;
+    justify-content: center;
     .ad-box {
-      display: none;
+      width: 160px;
+      height: 600px;
+      background-color: #555761;
     }
   }
+  .ad-box_row {
+    width: 924px;
+    height: 114px;
+    margin: 0 auto;
+    background-color: #555761;
+  }
   .main-content {
-    margin: 48px 0 0;
-    width: 100%;
-    .content-list {
-      .content-list-item {
-        flex-direction: column;
-        margin-top: 26px;
-        .card-wrapper {
-          margin: 0 auto 26px;
-          .card-img {
-            height: 400px;
+    margin: 124px 32px 0;
+    flex: 1;
+    max-width: 1400px;
+    .content-list-item {
+      display: flex;
+      &:not(:first-child) {
+        margin-top: 97px;
+      }
+      .card-wrapper {
+        margin-right: 137px;
+      }
+      .card-img {
+        height: 390px;
+        width: 220px;
+      }
+      .card-img-rotate {
+        transform: rotate(180deg);
+      }
+      .card-text {
+        text-align: center;
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 22px;
+        line-height: 30px;
+        color: rgba(255, 255, 255, 0.7);
+        margin-top: 12px;
+      }
+      .desc-title {
+        font-family: 'Cinzel Decorative';
+        font-style: normal;
+        font-weight: 700;
+        font-size: 36px;
+        line-height: 48px;
+      }
+      .desc-text {
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 28px;
+        margin-top: 40px;
+      }
+    }
+  }
+  .mt-48 {
+    margin-top: 48px;
+  }
+  .mt-200 {
+    margin-top: 200px;
+  }
+
+  .google_ad {
+    width: 924px;
+    height: 114px;
+    background-color: #555761;
+    margin: 48px auto;
+    overflow: hidden;
+  }
+}
+@media (max-width: 1050px) {
+  .tarot-container {
+    color: #fff;
+    padding: 40px 30px 0;
+    .title {
+      padding: 0;
+    }
+    .ad-box_row {
+      width: 100%;
+    }
+    .mt-48 {
+      margin-top: 48px;
+    }
+    .google_ad {
+      width: 100%;
+    }
+  }
+}
+@media (max-width: 750px) {
+  $pr: math.div(1vw, 3.75);
+  .tarot-container {
+    padding: 44 * $pr 16 * $pr 0;
+    .ad-box_row {
+      width: 100%;
+      height: 300 * $pr;
+      &.title_ad {
+        height: 114 * $pr;
+      }
+    }
+    .title {
+      font-family: 'Cinzel Decorative';
+      font-style: normal;
+      font-weight: 700;
+      font-size: 36 * $pr;
+      line-height: 48 * $pr;
+    }
+    .tarot-section {
+      .ad-box {
+        display: none;
+      }
+    }
+    .main-content {
+      margin: 0;
+      width: 100%;
+      .content-list {
+        .content-list-item {
+          flex-direction: column;
+          margin-top: 48 * $pr;
+          .card-wrapper {
+            margin: 0 auto 26 * $pr;
+            .card-img {
+              width: 220 * $pr;
+              height: 390 * $pr;
+            }
+          }
+          .card-text {
+            font-family: 'Rufina';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 18 * $pr;
+            line-height: 30 * $pr;
+          }
+          .desc-title {
+            font-family: 'Rubik';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 22 * $pr;
+            line-height: 30 * $pr;
+          }
+          .desc-text {
+            font-family: 'Rubik';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 14 * $pr;
+            line-height: 18 * $pr;
+            color: rgba(255, 255, 255, 0.7);
+            margin-top: 8 * $pr;
           }
         }
-        .card-text {
-          font-family: 'Rufina';
-          font-style: normal;
-          font-weight: 400;
-          font-size: 18px;
-          line-height: 30px;
-        }
-        .desc-title {
-          font-family: 'Rubik';
-          font-style: normal;
-          font-weight: 400;
-          font-size: 22px;
-          line-height: 30px;
-        }
-        .desc-text {
-          font-family: 'Rubik';
-          font-style: normal;
-          font-weight: 400;
-          font-size: 14px;
-          line-height: 18px;
-          color: rgba(255, 255, 255, 0.7);
-          margin-top: 8px;
-        }
       }
+    }
+    .mt-48 {
+      margin-top: 48 * $pr;
+    }
+    .google_ad {
+      width: 100%;
+      height: 299 * $pr;
+      background-color: #555761;
+      margin: 48 * $pr auto;
+      overflow: hidden;
     }
   }
 }
