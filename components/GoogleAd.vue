13a3160875f727cea7ef25ebd7346f6ae25c0fc0
Amber <89457376@qq.com>
2023-07-06 22:02:34 +0800

    移动端全宽
    移动端全宽

--- components/GoogleAd.vue
+++ components/GoogleAd.vue
@@ -11,7 +11,7 @@
       }"
       data-ad-client="ca-pub-6430486603399192"
       :data-ad-slot="id"
-      data-full-width-responsive="false"
+      data-full-width-responsive="true"
     ></ins>
   </div>
 </template>
